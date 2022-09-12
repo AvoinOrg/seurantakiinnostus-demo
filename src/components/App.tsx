d5f599717fe32c53bc15307a5b06a5174a2b1e9d
xbexbex <petja.valkama@gmail.com>
2022-09-12 21:58:38 +0300

    Add query client

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Theme, GlobalStyle } from '../styles';
 import { StateProvider } from '../components/State';
 import Main from './Main';
 
+const queryClient = new QueryClient();
+
 const App: React.FC<{}> = ({}) => {
   return (
-    <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <StateProvider>
-        <Main></Main>
-      </StateProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <StateProvider>
+          <Main></Main>
+        </StateProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
