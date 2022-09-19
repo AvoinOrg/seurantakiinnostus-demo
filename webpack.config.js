f68ccc499eb674ef865349bc9513a01410ad90da
xbexbex <petja.valkama@gmail.com>
2022-09-19 14:38:51 +0300

    Update webpack routing

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,11 @@ module.exports = {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  devServer: {
+    historyApiFallback: true,
   },
 
   resolve: {
