e482f9ff43680f1f8cdd93f86abd49ba3dbbf727
xbexbex <petja.valkama@gmail.com>
2022-10-05 20:30:28 +0300

    Add mui datepicker

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { Theme, GlobalStyle } from '../styles';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
 import { StateProvider } from '../components/State';
 import Main from './Main';
 
@@ -12,12 +14,14 @@ const queryClient = new QueryClient();
 const App: React.FC<{}> = ({}) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={Theme}>
-        <GlobalStyle />
-        <StateProvider>
-          <Main></Main>
-        </StateProvider>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyle />
+          <StateProvider>
+            <Main></Main>
+          </StateProvider>
+        </ThemeProvider>
+      </LocalizationProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
