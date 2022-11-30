1df4fad57f3edfc9e70499fca240c9c5b05424a2
xbexbex <petja.valkama@gmail.com>
2022-11-30 15:31:01 +0200

    Add widgets

---
+++ src/utils/widgets/anticipatedInputWidget.ts
@@ -0,0 +1,25 @@
+import baseWidget from './baseWidget';
+
+const anticipatedInputWidget = (apiKey, extraParams) => {
+  const serviceId = 'monint_basicsite_service_code_201911180952014';
+
+  const widgetParams = `
+    data-type="SingleServiceQuestionnaire"
+    data-show-service_name="true"
+    data-show-service_description="true"
+    data-show-map="true"
+    data-map-height="300"
+    data-show-obses="true"
+    data-obses-max_age="90"
+    data-obses-radius="9"
+    data-obses-label=""
+    data-obses-color=""
+    data-obses-cluster="true"
+    data-show-questionnaire="true"
+    data-images-count="2"
+  `;
+
+  return baseWidget(serviceId, apiKey, widgetParams, extraParams);
+};
+
+export default anticipatedInputWidget;
---
+++ src/utils/widgets/reportsWidget.ts
@@ -0,0 +1,25 @@
+import baseWidget from './baseWidget';
+
+const reportsWidget = (apiKey, extraParams) => {
+  const serviceId = 'csepin_localreport_service_code_en_202210171609144';
+
+  const widgetParams = `
+    data-type="SingleServiceQuestionnaire"
+    data-show-service_name="true"
+    data-show-service_description="true"
+    data-show-map="true"
+    data-map-height="300"
+    data-show-obses="true"
+    data-obses-max_age="90"
+    data-obses-radius="9"
+    data-obses-label=""
+    data-obses-color=""
+    data-obses-cluster="true"
+    data-show-questionnaire="true"
+    data-images-count="2"
+  `;
+
+  return baseWidget(serviceId, apiKey, widgetParams, extraParams);
+};
+
+export default reportsWidget;
