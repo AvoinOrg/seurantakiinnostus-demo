b3ae9984876246629bb0dbb7aea7c306dbbcc52b
xbexbex <petja.valkama@gmail.com>
2022-11-30 15:31:13 +0200

    Add buttons for widgets

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -8,71 +8,109 @@ import { getUnixTime } from 'date-fns';
 import { theme } from '../styles';
 import { StateContext } from '../components/State';
 import TextField from '@mui/material/TextField';
+import anticipatedInputWidget from '../utils/widgets/anticipatedInputWidget';
+import reportsWidget from '../utils/widgets/reportsWidget';
+import siteWidget from '../utils/widgets/siteWidget';
+import triggeringEventWidget from '../utils/widgets/triggeringEventWidget';
 
 function ControlPanel(): React.ReactElement {
   const {
+    handleModalClick,
     priority,
     paramApiId,
     paramEditorApiPriorityLevel,
     selectedDate,
     setSelectedDate,
     updateSearchParams,
+    apiKey,
+    extraParams,
   }: any = useContext(StateContext);
 
+  const handleAnticipatedClick = () => {
+    const newWidget = anticipatedInputWidget(apiKey, extraParams.citobsParams);
+    handleModalClick('anticipated', newWidget);
+  };
+
+  const handleReportsClick = () => {
+    const newWidget = reportsWidget(apiKey, extraParams.citobsParams);
+    handleModalClick('reports', newWidget);
+  };
+
+  const handleSiteCick = () => {
+    const newWidget = siteWidget(apiKey, extraParams.citobsParams);
+    handleModalClick('site', newWidget);
+  };
+
+  const handleTriggeringClick = () => {
+    const newWidget = triggeringEventWidget(apiKey, extraParams.citobsParams);
+    handleModalClick('triggering', newWidget);
+  };
+
   return (
-    <Container>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DateTimePicker
-          // displayStaticWrapperAs="desktop"
-          openTo="year"
-          value={selectedDate}
-          ampm={false}
-          ampmInClock={false}
-          inputFormat={'dd/MM/yyyy HH:mm'}
-          onChange={(newValue) => {
-            setSelectedDate(newValue);
-            updateSearchParams({ time: getUnixTime(newValue) });
-          }}
-          PaperProps={{
-            sx: {
-              margin: '10px -25px 0 0',
-              zIndex: 2500,
-            },
-          }}
-          PopperProps={{
-            sx: {
-              zIndex: 2500,
-            },
-          }}
-          renderInput={(params) => (
-            <TextField
-              sx={{
-                bgcolor: 'background.paper',
-                boxShadow: 1,
-                borderRadius: 2,
-                minWidth: 300,
-              }}
-              {...params}
-            />
-          )}
-        />
-      </LocalizationProvider>
-      <InfoContainer>
-        <InfoRow>
-          System API-ID:&nbsp;<b>{paramApiId === '' ? 'none' : paramApiId}</b>
-        </InfoRow>
-        <InfoRow>
-          Priority:&nbsp;<b>{priority == null ? 'loading..' : priority}</b>
-        </InfoRow>
-        {paramEditorApiPriorityLevel != null && (
+    <>
+      <TopRightContainer>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DateTimePicker
+            // displayStaticWrapperAs="desktop"
+            openTo="year"
+            value={selectedDate}
+            ampm={false}
+            ampmInClock={false}
+            inputFormat={'dd/MM/yyyy HH:mm'}
+            onChange={(newValue) => {
+              setSelectedDate(newValue);
+              updateSearchParams({ time: getUnixTime(newValue) });
+            }}
+            PaperProps={{
+              sx: {
+                margin: '10px -25px 0 0',
+                zIndex: 2500,
+              },
+            }}
+            PopperProps={{
+              sx: {
+                zIndex: 2500,
+              },
+            }}
+            renderInput={(params) => (
+              <TextField
+                sx={{
+                  bgcolor: 'background.paper',
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  minWidth: 300,
+                }}
+                {...params}
+              />
+            )}
+          />
+        </LocalizationProvider>
+        <InfoContainer>
           <InfoRow>
-            Editor Priority:&nbsp;
-            <b>{paramEditorApiPriorityLevel}</b>
+            System API-ID:&nbsp;<b>{paramApiId === '' ? 'none' : paramApiId}</b>
           </InfoRow>
-        )}
-      </InfoContainer>
-      {/* <Button>Import</Button> */}
-    </Container>
+          <InfoRow>
+            Priority:&nbsp;<b>{priority == null ? 'loading..' : priority}</b>
+          </InfoRow>
+          {paramEditorApiPriorityLevel != null && (
+            <InfoRow>
+              Editor Priority:&nbsp;
+              <b>{paramEditorApiPriorityLevel}</b>
+            </InfoRow>
+          )}
+        </InfoContainer>
+      </TopRightContainer>
+      <BottomRightContainer>
+        <Button onClick={handleSiteCick}>Setting monitoring site</Button>
+        <Button onClick={handleAnticipatedClick}>
+          Setting anticipated data input service type
+        </Button>
+        <Button onClick={handleTriggeringClick}>
+          Setting triggering event
+        </Button>
+        <Button onClick={handleReportsClick}>Reports and requests</Button>
+      </BottomRightContainer>
+    </>
   );
 }
 
@@ -97,10 +135,11 @@ const Button: any = styled.div`
   color: ${theme.palette.colors.black};
   border-bottom: 1px solid #ccc;
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.55);
-  height: 30px;
+  height: 40px;
   border-radius: 2px;
   margin: 0 0 10px 0;
   padding: 0 10px 0 5px;
+  line-height: 40px;
 
   &:hover {
     cursor: pointer;
@@ -108,9 +147,25 @@ const Button: any = styled.div`
   }
 `;
 
-const Container: any = styled.div`
+const TopRightContainer: any = styled.div`
   position: fixed;
   top: 10px;
+  right: 10px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  z-index: 2000;
+  font: bold 18px;
+  font-size: 18px;
+  font-family: ${theme.typography.primary};
+  line-height: 30px;
+  text-indent: 1px;
+  justify-content: flex-end;
+`;
+
+const BottomRightContainer: any = styled.div`
+  position: fixed;
+  bottom: 10px;
   right: 10px;
   display: flex;
   flex-direction: column;
